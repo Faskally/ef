13b836bf9e7c15f0ff5e31427d1c3834cf75851c
colinpmillar <millarc@marlab.ac.uk>
2015-08-21 11:10:14 +0100

    more build check fixes

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,12 @@
 
 
+#' Inverse logistic transformation
+#'
+#' This function transforms values on the logistic scale to values on
+#' the probability scale.
+#' 
+#' @param x a numeric vector
+#' @return vector of values between 0 and 1
 #' @export
 invlogit <- function(x) {
   1/(1 + exp(-x))
--- R/zzz.R
+++ R/zzz.R
@@ -7,27 +7,28 @@
 {
   packageStartupMessage("Building optimiser for first use...")
 
-  stanmod <- rstan::stan_model(model_code = "
-      data {
-        int<lower=0> N; // number of observations
-        int<lower=0> K; // number of parameters
-        row_vector[N] y[3]; // data - 3 passes 
-        matrix[N,K] A[3]; // the design matrices - 3 passes 
-      }
-      parameters {
-        vector[K] alpha;
-      } 
-      model {
-        vector[N] p[3]; // calculate all the probs required
-        for (k in 1:3) {
-          p[k] <- 1.0 ./ (1.0 + exp(-1.0 * A[k] * alpha)); 
+  tmp <- capture.output(
+    stanmod <- rstan::stan_model(model_code = "
+        data {
+          int<lower=0> N; // number of observations
+          int<lower=0> K; // number of parameters
+          row_vector[N] y[3]; // data - 3 passes 
+          matrix[N,K] A[3]; // the design matrices - 3 passes 
         }
-        increment_log_prob(y[1] * log(p[1])); 
-        increment_log_prob(y[2] * log((1-p[1]) .* p[2]));
-        increment_log_prob(y[3] * log((1-p[1]) .* (1-p[2]) .* p[3]));
-        increment_log_prob(-1.0 * (y[1] + y[2] + y[3]) * log(p[1] + (1-p[1]) .* p[2] + (1-p[1]) .* (1-p[2]) .* p[3]));
-      }")
-
+        parameters {
+          vector[K] alpha;
+        } 
+        model {
+          vector[N] p[3]; // calculate all the probs required
+          for (k in 1:3) {
+            p[k] <- 1.0 ./ (1.0 + exp(-1.0 * A[k] * alpha)); 
+          }
+          increment_log_prob(y[1] * log(p[1])); 
+          increment_log_prob(y[2] * log((1-p[1]) .* p[2]));
+          increment_log_prob(y[3] * log((1-p[1]) .* (1-p[2]) .* p[3]));
+          increment_log_prob(-1.0 * (y[1] + y[2] + y[3]) * log(p[1] + (1-p[1]) .* p[2] + (1-p[1]) .* (1-p[2]) .* p[3]));
+        }")
+  )
   assign("stanmod", stanmod, .efEnv)
 }
 
