9eeb65c21afcf0c1e0e5bb975ec6f5c8886a296c
Colin Millar <colinpmillar@gmail.com>
2015-11-08 20:49:03 +0000

    fixed prolem with different passes per fishing

--- R/Likelihoods_and_gradients.R
+++ R/Likelihoods_and_gradients.R
@@ -1,7 +1,7 @@
 
 #
 # Purpose: fit capture probabilities and abundances to electrofishing data
-# 
+#
 # author: CP Millar, millarc@marlab.ac.uk
 # origional date: 12/2014
 #
@@ -9,8 +9,8 @@
 #' Estimate capture probabilites from electrofishing data
 #'
 #' This function uses the marginal likelihood of capture probabilities
-#' to estimate model parameters 
-#' 
+#' to estimate model parameters
+#'
 #'
 #' @param formula a formula object
 #' @param data a data.frame containing all relavent info
@@ -20,9 +20,7 @@
 #' @param init should initialisatiom be random?
 #' @param hessian if TRUE the hessian is computed and the covariance matrix of the parameters is returned via Vb
 #' @return glm type object
-#' @export
-efp <- function(formula, data = NULL, pass, offset = NULL, verbose = FALSE, init = "0", hessian = TRUE) {
-
+efp_old <- function(formula, data = NULL, pass, offset = NULL, verbose = FALSE, init = "0", hessian = TRUE) {
 
   .buildOptimiser()
 
@@ -35,7 +33,7 @@ efp <- function(formula, data = NULL, pass, offset = NULL, verbose = FALSE, init
   pass <- eval(pass, data, environment(formula))
   if (length(unique(pass)) != 3 || !all(sort(unique(pass)) == 1:3)) stop("There should only be 3 passes and they should be numbered 1 to 3")
   # the within sample structure is then,
-  X <- model.matrix(~ factor(pass) - 1)
+  X <- model.matrix(~ factor(pass)  - 1)
 
   # set up offset
   if (is.null(offset)) {
@@ -46,7 +44,7 @@ efp <- function(formula, data = NULL, pass, offset = NULL, verbose = FALSE, init
   Gsetup <- gam(formula, data = data, fit = FALSE)
   G <- Gsetup $ X
   if (nrow(G) != nrow(data)) stop("I think there are NAs in your data, please check and remove them before rerunning.")
- 
+
   # remove redundant / collinear parameters
   qr.G <- qr(G)
   rank.deficient <- qr.G $ pivot[abs(diag(qr.G $ qr)) < 1e-7]
@@ -75,21 +73,21 @@ efp <- function(formula, data = NULL, pass, offset = NULL, verbose = FALSE, init
   dim(A) <- c(K, N, 3)
   A <- aperm(A, c(3,2,1))
 
-  standat <- 
-    list(N = N,  
+  standat <-
+    list(N = N,
          K = K,
          y = y,
          A = A,
          offset = offset)
 
   if (!verbose) {
-    tmp <- 
+    tmp <-
       capture.output(
         opt <- rstan::optimizing(.efEnv $ stanmod, data = standat, algorith = "BFGS", hessian = hessian, verbose = verbose, init = init)
       )
   } else {
     opt <- rstan::optimizing(.efEnv $ stanmod, data = standat, algorith = "BFGS", hessian = hessian, verbose = verbose, init = init)
-  } 
+  }
 
   opt $ formula <- formula # for printing and summary
   opt $ llik <- opt $ value
@@ -106,7 +104,7 @@ efp <- function(formula, data = NULL, pass, offset = NULL, verbose = FALSE, init
   opt $ fitted <- p <- transpar(opt $ par, Gfit)
   opt $ residuals <- rep(0, nrow(data))
   opt $ null.deviance <- NA
-  opt $ deviance <- NA 
+  opt $ deviance <- NA
   opt $ family <- binomial()
   if (hessian) rownames(opt $ hessian) <- colnames(opt $ hessian) <- colnames(Gfit)
   opt $ Vb <- if (hessian) try(solve(-1 * opt $ hessian)) else NULL
@@ -114,7 +112,7 @@ efp <- function(formula, data = NULL, pass, offset = NULL, verbose = FALSE, init
 
   # get a gam container
   # g1 <- gam(G = Gsetup)
-  # g1 $ coefficients[] <- opt $ par       
+  # g1 $ coefficients[] <- opt $ par
   # g1 $ Vp[] <- opt $ Vb
   # g1 $ family <- binomial()
   # X <- predict(g1, type = "lpmatrix")
@@ -130,13 +128,198 @@ efp <- function(formula, data = NULL, pass, offset = NULL, verbose = FALSE, init
 
 
 
-#' Utility function to convert parameters to probabilities 
+#' Estimate capture probabilites from electrofishing data
 #'
-#' The matrix G shoudl be of dimension n x p, 
+#' This function uses the marginal likelihood of capture probabilities
+#' to estimate model parameters
+#'
+#'
+#' @param formula a formula object
+#' @param data a data.frame containing all relavent info
+#' @param pass a vector of integers giving the pass number of the observation
+#' @param id a vector of integers identifying an observation (a set of electrofishing passes)
+#' @param offset an possible offset for the linear predictor of capture probability
+#' @param verbose if TRUE stan optimiser messages are printed to the screen
+#' @param init should initialisatiom be random?
+#' @param hessian if TRUE the hessian is computed and the covariance matrix of the parameters is returned via Vb
+#' @param fit if TRUE model is fitted if FALSE the data that would be passed to the optimiser is returned
+#' @return glm type object
+# create two electrofishing site visits with 3 and 4 passes and 2 lifestages
+ef_data <- data.frame(n      = c(100, 53, 24, 50, 26, 12,
+                                 100, 53, 24, 50, 26, 12),
+                      pass   = c(  1,  2,  3,  1,  2,  3,
+                                   1,  2,  3,  1,  2,  3),
+                      stage  = c(  1,  1,  1,  2,  2,  2,
+                                   1,  1,  1,  2,  2,  2),
+                      sample = c(  1,  1,  1,  2,  2,  2,
+                                   3,  3,  3,  4,  4,  4))
+
+ef_data2 <- data.frame(n      = c(100, 53, 24, 50, 26, 12,
+                                  100, 53, 24, 12, 50, 26, 12, 6),
+                       pass   = c(  1,  2,  3,  1,  2,  3,
+                                    1,  2,  3,  4,  1,  2,  3, 4),
+                       stage  = c(  1,  1,  1,  2,  2,  2,
+                                    1,  1,  1,  1,  2,  2,  2, 2),
+                       sample = c(  1,  1,  1,  2,  2,  2,
+                                    3,  3,  3,  3,  4,  4,  4, 4))
+
+ef_data3 <- data.frame(n      = c(100, 53, 24, 50, 26, 12, 40,
+                                  100, 53, 24, 12, 50, 26, 12, 6, 40),
+                       pass   = c(  1,  2,  3,  1,  2,  3, 1,
+                                    1,  2,  3,  4,  1,  2,  3, 4, 1),
+                       stage  = c(  1,  1,  1,  2,  2,  2, 1,
+                                    1,  1,  1,  1,  2,  2,  2, 2, 2),
+                       sample = c(  1,  1,  1,  2,  2,  2, 5,
+                                    3,  3,  3,  3,  4,  4,  4, 4, 6))
+
+
+# Fit a simple model
+devtools::load_all(pkg)
+m2 <- efp(n ~ 1 + factor(stage), data = ef_data, pass = pass, id = sample)
+cbind(ef_data, fit = fitted(m2))
+
+m3 <- efp(n ~ 1 + factor(stage), data = ef_data2, pass = pass, id = sample)
+cbind(ef_data2, fit = fitted(m3))
+
+m4 <- efp(n ~ 1 + factor(stage), data = ef_data3, pass = pass, id = sample)
+cbind(ef_data3, fit = fitted(m4))
+efp <- function(formula, data = NULL, pass, id, offset = NULL, verbose = FALSE, init = "0", hessian = TRUE, fit = TRUE) {
+
+  .buildOptimiser2()
+
+  # some checks
+  if (is.null(data)) stop("must supply data")
+
+  # get pass information
+  if (missing(pass)) stop("must supply pass number")
+  pass <- substitute(pass)
+  pass <- eval(pass, data, environment(formula))
+  if (missing(id)) stop("must supply sample id")
+  id <- substitute(id)
+  id <- eval(id, data, environment(formula))
+  # a check of somekind? there should only be 1 pass of each pass number per sample!
+  # if (length(unique(pass)) != 3 || !all(sort(unique(pass)) == 1:3)) stop("There should only be 3 passes and they should be numbered 1 to 3")
+  # sort data by sample id then pass?
+  # the within sample structure is then,
+  Xs <- model.matrix(~ factor(id)  - 1)
+  Xp <- model.matrix(~ factor(pass)  - 1)
+
+  # set up offset
+  if (is.null(offset)) {
+    offset <- rep(0, nrow(data))
+  }
+
+  # set up model
+  Gsetup <- gam(formula, data = data, fit = FALSE)
+  G <- Gsetup $ X
+  if (nrow(G) != nrow(data)) stop("I think there are NAs in your data, please check and remove them before rerunning.")
+
+  # remove redundant / collinear parameters
+  qr.G <- qr(G)
+  rank.deficient <- qr.G $ pivot[abs(diag(qr.G $ qr)) < 1e-7]
+  if (length(rank.deficient)) {
+    droppar <- paste(colnames(G)[rank.deficient], collapse = "\n\t")
+    warning("*** Model has ", length(rank.deficient)," too many parameter(s)!!\n    i will remove the redundant ones:\n\t", droppar, call. = FALSE)
+    Gfit <- G[,-rank.deficient]
+  } else {
+    Gfit <- G
+  }
+
+  # define inputs for likelihood
+  npasses <- colSums(Xs)
+  N <- ncol(Xs)
+  K <- ncol(Gfit)
+  s <- max(npasses)
+
+  # get data in the correct order
+  mat <- data.frame(pass = pass, id = id, y = Gsetup$y, offset = offset, irow = 1:length(pass))
+  mat2 <- expand.grid(id = 1:ncol(Xs), pass = 1:s)
+  mat2 <- merge(mat2, mat, all.x = TRUE)
+  mat2[is.na(mat2$y), c("y", "offset")] <- 0
+
+  # we want observartions in columns, passes in rows
+  y <- matrix(mat2$y, nrow = s, ncol = N)
+  y_tot <- colSums(y)
+
+  # same for offset
+  offset <- matrix(mat2$offset, nrow = s, ncol = N)
+
+  # same for design matrix, but this is a bit trickier
+  A <- array(0, c(s, N, K))
+  for (i in 1:N) {
+    .irow <- subset(mat2, id == i)$irow
+    A[!is.na(.irow),i,] <- Gfit[.irow[!is.na(.irow)],]
+  }
+
+  standat <-
+    list(N = N,
+         K = K,
+         s = s,
+         npasses = npasses,
+         y = y,
+         T = y_tot,
+         offset = offset,
+         A = A)
+
+  if (!fit) return(standat)
+
+  if (!verbose) {
+    tmp <-
+      capture.output(
+        opt <- rstan::optimizing(.efEnv $ stanmod2, data = standat, algorith = "BFGS", hessian = hessian, verbose = verbose, init = init)
+      )
+  } else {
+    opt <- rstan::optimizing(.efEnv $ stanmod2, data = standat, algorith = "BFGS", hessian = hessian, verbose = verbose, init = init)
+  }
+
+  opt $ formula <- formula # for printing and summary
+  opt $ llik <- opt $ value
+  opt $ terms <- Gsetup $ terms
+  opt $ call <- match.call()
+  opt $ aic <- -2 * opt $ llik + 2 * ncol(Gfit)
+  opt $ G <- G
+  opt $ Gfit <- Gfit
+  opt $ coefficients <- opt $ par
+  names(opt $ coefficients) <- colnames(Gfit)
+  opt $ df.null <- nrow(G)
+  opt $ df.residual <- nrow(G) - ncol(Gfit)
+  opt $ rank <- ncol(Gfit)
+  opt $ fitted <- p <- transpar(opt $ par, Gfit)
+  opt $ residuals <- rep(0, nrow(data))
+  opt $ null.deviance <- NA
+  opt $ deviance <- NA
+  opt $ family <- binomial()
+  if (hessian) rownames(opt $ hessian) <- colnames(opt $ hessian) <- colnames(Gfit)
+  opt $ Vb <- if (hessian) try(solve(-1 * opt $ hessian)) else NULL
+  opt $ Gsetup <- Gsetup
+
+  # get a gam container
+  # g1 <- gam(G = Gsetup)
+  # g1 $ coefficients[] <- opt $ par
+  # g1 $ Vp[] <- opt $ Vb
+  # g1 $ family <- binomial()
+  # X <- predict(g1, type = "lpmatrix")
+  # g1 $ linear.predictors <-  c(X %*% g1 $ coef)
+  # g1 $ fitted.values <- c(1/(1 + exp(-g1 $ linear.predictors)))
+  # g1 $ aic <- opt $ aic
+
+  class(opt) <- c("efp", "glm", "lm")
+  opt
+}
+
+
+
+
+
+
+
+#' Utility function to convert parameters to probabilities
+#'
+#' The matrix G shoudl be of dimension n x p,
 #' and the parameter vector should be lenght p
 #'
 #' @param par fitted model parameters
-#' @param G The design matrix for a model 
+#' @param G The design matrix for a model
 #' @return a data frame
 #' @export
 transpar <- function(par, G) {
--- R/zzz.R
+++ R/zzz.R
@@ -14,19 +14,19 @@
       data {
         int<lower=0> N; // number of observations
         int<lower=0> K; // number of parameters
-        row_vector[N] y[3]; // data - 3 passes 
-        vector[N] offset[3]; // offset - 3 passes 
-        matrix[N,K] A[3]; // the design matrices - 3 passes 
+        row_vector[N] y[3]; // data - 3 passes
+        vector[N] offset[3]; // offset - 3 passes
+        matrix[N,K] A[3]; // the design matrices - 3 passes
       }
       parameters {
         vector[K] alpha;
-      } 
+      }
       model {
         vector[N] p[3]; // calculate all the probs required
         for (s in 1:3) {
-          p[s] <- 1.0 ./ (1.0 + exp(-1.0 * A[s] * alpha - offset[s])); 
+          p[s] <- 1.0 ./ (1.0 + exp(-1.0 * A[s] * alpha - offset[s]));
         }
-        increment_log_prob(y[1] * log(p[1])); 
+        increment_log_prob(y[1] * log(p[1]));
         increment_log_prob(y[2] * log((1-p[1]) .* p[2]));
         increment_log_prob(y[3] * log((1-p[1]) .* (1-p[2]) .* p[3]));
         increment_log_prob(-1.0 * (y[1] + y[2] + y[3]) * log(p[1] + (1-p[1]) .* p[2] + (1-p[1]) .* (1-p[2]) .* p[3]));
@@ -37,8 +37,62 @@
   invisible(TRUE)
 }
 
+.buildOptimiser2 <- function(force = FALSE) {
 
-##.onLoad <- function(libname, pkgname) 
+  if (exists("stanmod2", envir = .efEnv) & !force) return (invisible(FALSE))
+
+  message("Building optimiser for first use...")
+
+  stanmod2 <- rstan::stan_model(model_code = "
+    data {
+      int<lower=0> N; // number of multipass observations
+      int<lower=0> K; // number of parameters
+      int<lower=0> s; // max number of passes
+      int<lower=0> npasses[N]; // number of passes per observation - vectors of s passes
+      row_vector[N] y[s]; // data - vectors of s passes
+      row_vector[N] T; // data - total catch
+      vector[N] offset[s]; // offset - s passes
+      matrix[N,K] A[s]; // the design matrices - one for each pass
+    }
+    parameters {
+       vector[K] alpha;
+    }
+    model {
+      vector[N] p[s];
+      vector[N] pi[s];
+      vector[N] piT;
+      // calculate all the within pass probs required
+      for (i in 1:s) {
+        p[i] <- 1.0 ./ (1.0 + exp(-1.0 * A[i] * alpha - offset[i]));
+      }
+      // calculate all the marginal(?) probs required
+      pi[1] <- p[1];
+      for (i in 2:s) {
+        pi[i] <- p[i];
+        for (j in 1:(i-1)) {
+          pi[i] <- pi[i] .* (1-p[j]);
+        }
+      }
+      // calculate the probability of capture - needs number of passes
+      piT <- pi[1];
+      for (i in 1:N) {
+        for (j in 2:npasses[i]) {
+          piT[i] <- piT[i] + pi[j,i];
+        }
+      }
+      // calculate the probability of capture - needs number of passes
+      for (i in 1:s) {
+        increment_log_prob(y[i] * log(pi[i]));
+      }
+      increment_log_prob(-1.0 * T * log(piT));
+    }")
+
+  assign("stanmod2", stanmod2, .efEnv)
+
+  invisible(TRUE)
+}
+
+##.onLoad <- function(libname, pkgname)
 ##{
 ##  #packageStartupMessage("Building optimiser for first use...")
 ##
