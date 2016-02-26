44f8fb1923f2c9267e2f276c344055e506f5caaa
Colin Millar <colinpmillar@googlemail.com>
2016-02-26 16:16:25 +0000

    added as.gam function
    and improved fitted values extraction functions

--- R/Likelihoods_and_gradients.R
+++ R/Likelihoods_and_gradients.R
@@ -236,18 +236,51 @@ simulate.efp <- function(object, nsim = 1000, seed = NULL, ...) {
 
 
 #' @export
-fitted.efp <- function (object, type = c("p", "pi", "both"), ...)
+fitted.efp <- function (object, type = "p", ...)
 {
+  type <- match.arg(type, c("p", "pi", "both", "lpmatrix"))
   if (type == "p") {
     object $ p
   } else if (type == "pi") {
     object $ pi
   } else if (type == "both") {
     object[c("p", "pi")]
+  } else if (type == "lpmatrix") {
+    object $ G
   }
 }
 
+#' @export
+as.gam <- function(object) {
+  stopifnot(is(object)[1] == "efp")
+  # make a gam container from the originoal setup
+  g1 <- mgcv::gam(G = object $ Gsetup)
 
+  # recheck for rank deficiency
+  qr.G <- qr(object $ G)
+  rank.deficient <- qr.G $ pivot[abs(diag(qr.G $ qr)) < 1e-7]
+  whichkeep <- -rank.deficient
+  if (!length(whichkeep)) whichkeep <- 1:length(object $ coefficients)
+  redudant <- names(g1 $ coefficients[-1 * whichkeep])
+  if (length(redudant)) {
+    warning("there is some reduandancy in the model specification.  This may be fine, but best to check.")
+  }
+
+  # assign coefficients to gam container
+  g1 $ coefficients[] <- 0
+  g1 $ coefficients[whichkeep] <- m0 $ coefficients
+
+  # assign variance matrix to gam container
+  g1 $ Vp[] <- 0
+  diag(g1 $ Vp[]) <- 1e-5
+  g1 $ Vp[whichkeep, whichkeep] <- m0 $ Vb
+
+  # assign binomial family to gam container
+  g1 $ family <- binomial()
+
+  message("Note that, although predictions and plots will be correct\n smoothing parameter estimates are all wrong as no smoothing took place!")
+  g1
+}
 
 
 
