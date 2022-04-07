a54b99c5bba5a5ae960977463ed95b6cecb32509
Iain A Malcolm <i.a.malcolm@marlab.ac.uk>
2022-04-07 15:02:48 +0100

    Function updated so that visitID information comes from model object and correction on the indexing for where estimates of overdispersion come from.

--- R/BICadj.R
+++ R/BICadj.R
@@ -21,6 +21,6 @@ BICadj <- function(model, data, overdispersion.output) {
   # overdispersion (within or between visits), which makes it more stringent 
   # when adding or removing terms from the model.
 
-  -2 * model$llik / max(overdispersion.output[c("sitevisit", "large"), "deviance"]) +
-   log(length(unique(paste(data$siteID, data$date)))) * length(model$coefficients)
+  -2 * model$llik / max(overdispersion.output[c("sitevisit", "large"), "disp"]) +
+   log(length(unique(model$data$visitID))) * length(model$coefficients)
 }
